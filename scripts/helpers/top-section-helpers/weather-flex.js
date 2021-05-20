498c9137d084bc051e71fcb196a44f5ba59b77b4
avinishanth <avinishanth79@gmail.com>
2021-05-20 17:35:28 +0530

    iubgb

--- scripts/helpers/top-section-helpers/weather-flex.js
+++ scripts/helpers/top-section-helpers/weather-flex.js
@@ -1,4 +1,4 @@
-import {tempConditionFunc} from './temperature-condition.js';
+import {tempConditionFunc} from '../top-section-helpers/temperature-condition.js';
 
 export function weatherFlexFunc(tempFlexRef,weatherIconFlexRef,tempWeatherInfo,tempNextFiveHrsRef){
     //now flex
